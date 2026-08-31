9c3048b47d182e2e4fb65fc0e70b4ad2d1339be6
jayraj1011 <jay.raj.979899@gmail.com>
2026-08-31 22:40:35 +0530

    Time: 1194 ms (5.01%), Space: 410.8 MB (5.04%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
-    int beautySum(string s) {
-
-        int n = s.size();
-        int sumOfBeauty = 0;
-
-        for ( int i=0; i<n; i++ ) {
-            for ( int j=i; j<n; j++ ) {
-                vector<int> freq(26, 0);
-                for ( int k=i; k<=j; k++ ) {
-                    freq[s[k] - 'a']++;
-                }
-                int maxi = INT_MIN, mini = INT_MAX;
-                for ( int i=0; i<26; i++ ) {
-                    if (freq[i] == 0 ) continue; 
-                    maxi = max(maxi, freq[i]);
-                    mini = min(mini, freq[i]);
-                }
-                sumOfBeauty += (maxi - mini);
+    int beauty(string ss){
+        int freq[26]={0};
+        for(int i=0;i<ss.size();i++){
+            freq[ss[i]-'a']++;
+        }
+        int mx=INT_MIN;
+        int mn=INT_MAX;
+        for(int j=0;j<26;j++){
+            if(freq[j]>0){
+                mx=max(mx,freq[j]);
+                mn=min(mn,freq[j]);
             }
         }
-
-        return sumOfBeauty;
-
+        return mx-mn;
     }
+    int beautySum(string s) {
+        int sum=0;
+        int left=0;
+        int right=1;
+        while(left<s.size() && right<s.size()){
+            while(right!=s.size()){
+                sum+=beauty(s.substr(left,right-left+1));
+                right++;
+            }
+            if(right==s.size()){
+                left++;
+                right=left+1;
+            }
+        }
+        return sum;
+    };
 };
