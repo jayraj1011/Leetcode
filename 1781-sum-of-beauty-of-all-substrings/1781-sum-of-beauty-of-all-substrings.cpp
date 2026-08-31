794b1431d8d469fbf11b99a22b9d84d86b1296d0
jayraj1011 <jay.raj.979899@gmail.com>
2026-08-31 22:53:25 +0530

    Time: 119 ms (41.88%), Space: 8.8 MB (91.01%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
-    int beauty(string ss){
-        int freq[26]={0};
-        for(int i=0;i<ss.size();i++){
-            freq[ss[i]-'a']++;
-        }
-        int mx=INT_MIN;
-        int mn=INT_MAX;
-        for(int j=0;j<26;j++){
-            if(freq[j]>0){
-                mx=max(mx,freq[j]);
-                mn=min(mn,freq[j]);
-            }
-        }
-        return mx-mn;
-    }
+    
     int beautySum(string s) {
         int sum=0;
-        int left=0;
-        int right=1;
-        while(left<s.size() && right<s.size()){
-            while(right!=s.size()){
-                sum+=beauty(s.substr(left,right-left+1));
-                right++;
-            }
-            if(right==s.size()){
-                left++;
-                right=left+1;
+        for(int i=0;i<s.size();i++){
+            int frq[26]={0};
+            for(int j=i;j<s.size();j++){
+                frq[s[j]-'a']++;
+                int mx=INT_MIN;
+                int mn=INT_MAX;
+                for(int k=0;k<26;k++){
+                    if(frq[k]>0){
+                        mx=max(mx,frq[k]);
+                        mn=min(mn,frq[k]);
+                    } 
+                }
+                sum+=mx-mn;
             }
         }
         return sum;
